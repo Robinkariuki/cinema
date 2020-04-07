61bf364834afd9503b4e7597f4dd6302d0b952f5
Robinkariuki <robinkaris7@gmail.com>
2020-04-07 13:46:43 +0300

    made changes to home component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './header/Header';
+import Home from './home/Home';
 
 const App = ()=>{
     return(
         <div>
             <Header/>
+            <Home/>
 
         </div>
     )
---
+++ src/components/home/Home.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+
+class Home extends Component {
+    state = {
+
+    }
+    render(){
+        return(
+            <div>
+                <h1>home</h1>
+                
+            </div>
+        )
+    }
+}
+
+
+export default Home;
