f59e40531e7c28cc036d3a6ae71dbde9995c6e42
Robinkariuki <robinkaris7@gmail.com>
2020-04-08 16:18:27 +0300

    added logic for loading items

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './Home.css';
+import {API_KEY,API_Url,Image_Base_Url,Backdrop_Size,Poster_Size}from'../../config';
 import Heroimage from '../elements/HeroImage/Heroimage';
 import Searchbar from '../elements/Searchbar/searchbar';
 import fourcolgrid from '../elements/fourcolgrid/FourColGrid';
@@ -10,9 +11,44 @@ import FourColGrid from '../elements/fourcolgrid/FourColGrid';
 
 class Home extends Component {
     state = {
-
+        movies :[],
+        heroImage : null,
+        loading : false,
+        currentpage : 0,
+        totalpages : 0,
+        searchterm : ''
+ 
     }
-    
+    componentDidMount(){
+        this.setState({loading:true});
+        const endpoint = `${API_Url}/movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+        this.fetchItems(endpoint);
+    }
+    loadmoreitems =() =>{
+        let endpoint ='';
+        this.setState({loading:true});
+
+        if(this.state.search ===''){
+            endpoint =`${API_Url}/movie/popular?api_key=${API_KEY}&language=en-US&page=${this.state.currentpage+1}`
+        }else{
+            endpoint=`${API_Url}\search\movie?apikey=${API_KEY}&language=en-US&query${this.state.searchterm}&page=${this.state.currentpage+1}`
+        }
+        this.fetchItems(endpoint); 
+    } 
+    fetchItems = (endpoint) => {
+        fetch(endpoint)
+        .then(result => result.json())
+        .then(result =>{
+         this.setState({
+              movies :[...this.state.movies, ...result.results],
+              heroImage: this.state.heroImage || result.results[0],
+              loading:false,
+              currentpage:result.total_pages
+
+        });
+    })
+}
+
     render(){
         return(
             <div className='rmdb-home'>
--- src/config.js
+++ src/config.js
@@ -1,18 +1,18 @@
-const API_url = 'https://api.themoviedb.org/3';
+const API_Url = 'https://api.themoviedb.org/3';
 
 const API_KEY = '12fa40673f3d6c3c034962aaab93d7d2';
 
-const Image_Base_url ='https://image.tmdb.org/t/p';
+const Image_Base_Url ='https://image.tmdb.org/t/p';
 
 const Backdrop_Size = 'w1280';
 
-const Poster_size = 'w500';
+const Poster_Size = 'w500';
 
 
 export {
     API_KEY,
-    API_url,
-    Image_Base_url,
+    API_Url,
+    Image_Base_Url,
     Backdrop_Size,
-    Poster_size
+    Poster_Size
 }
