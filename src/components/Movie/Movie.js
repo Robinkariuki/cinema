5a57ed2ebcecb97e20418c5001c49bf9a0d4522b
Robinkariuki <robinkaris7@gmail.com>
2020-04-17 21:55:41 +0300

    added imports to movie component

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import { API_KEY,API_Url } from '../../config';
+import Navigation from '../elements/Navigation/Navigation';
+import Movieinfo from '../elements/MovieInfobar/MovieInfobar';
+import FourColgrid from '../elements/fourcolgrid/FourColGrid';
+import Actor from '../elements/Actor/Actor';
+import Spinner from'../elements/Spinner/Spinner';
+import './Movie.css';
+
 
 class Movie extends Component {
     render(){
         return(
             <div>
-                movie
+                
             </div>
         )
     }
