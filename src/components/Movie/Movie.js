3fc5d5620e5787f59443bcb5029704d94b4f061c
Robinkariuki <robinkaris7@gmail.com>
2020-04-18 12:29:02 +0300

    added methods to fecth endpoints

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -8,6 +8,7 @@ import Spinner from'../elements/Spinner/Spinner';
 import Info from '../elements/Info/info'
 
 import './Movie.css';
+import { JsxEmit } from 'typescript';
 
 
 class Movie extends Component {
@@ -18,13 +19,52 @@ class Movie extends Component {
         loading: false
      
     }
+    componentDidMount(){
+        this.setState({loading:true})
+        //fetch the movie..
+        const endpoint =`${API_Url}movie/${this.props.match.params.movieId}?api_key=${API_KEY}&language=en-US`;
+        this.fetchItems(endpoint);
+
+    }
+    fetchItems= (endpoint)=>{
+        fetch(endpoint)
+        .then(result => result.json())
+        .then(result => {
+            console.log(result)
+            if (result.status_code){
+                this.setState({loading:false})
+            }else{
+                this.setState({movie:result}, ()=>{
+                    const endpoint = `${API_Url}movie/${this.props.match.params.movieId}/credits?api_key=${API_KEY}`
+                    fetch(endpoint)
+                    .then(result => result.json())
+                    .then(result =>{
+                        const directors =result.crew.filter((member)=> member.job ==='Director');
+
+                        this.setState({
+                            actors:result.cast,
+                            directors,
+                            loading:false
+
+                        })
+                    })
+                } )
+            }
+          
+            
+        })
+        .catch(error =>console.error('Error',error))
+        
+
+    }
+
     render(){
         return(
             <div className='rmdb-movie'>
                 <Navigation/>
                 <Info/>
                 <MovieInfoBar/>
-                <FourColgrid/>
+                {/* <FourColgrid/> */}
                 <Spinner/>
 
                 
