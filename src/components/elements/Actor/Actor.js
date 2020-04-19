71edeb046c88acbf8144142b09bea8fd0c23e880
Robinkariuki <robinkaris7@gmail.com>
2020-04-19 12:47:58 +0300

    added logic to actor component

--- src/components/elements/Actor/Actor.js
+++ src/components/elements/Actor/Actor.js
@@ -4,9 +4,17 @@ import './Actor.css';
 
 
 const Actor = (props)=>{
+    const Poster_Size ='w154';
+
     return(
-        <div>
-            Actor
+        <div className='rmdb-actor'>
+            <img
+                src={props.actor.profile_path ? `${Image_Base_Url}${Poster_Size}${props.actor.profile_path}`: './images/no_image.jpg'}
+                alt ='actorthumb'
+                />
+        <span className ='rmdb-actor-name'>{props.actor.name}</span>
+        <span className ='rmdb-actor-characters'>{props.actor.character}</span>
+            
         </div>
     )
 }
