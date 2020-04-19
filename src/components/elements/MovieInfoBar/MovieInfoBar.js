0b6d1ee4789ac6926c208760965374ba4acaa105
Robinkariuki <robinkaris7@gmail.com>
2020-04-19 12:03:23 +0300

    added logic to the Movieinfobar component

--- src/components/elements/MovieInfoBar/MovieInfoBar.js
+++ src/components/elements/MovieInfoBar/MovieInfoBar.js
@@ -6,8 +6,21 @@ import {calcTime, convertMoney} from '../../../helpers';
 
 const MovieInfoBar = ()=>{
     return(
-        <div>
-            Movie info bar
+        <div className='rmdb-movieinfobar'>
+            <div className='rmdb-movieinfobar-content'>
+                <div className='rmdb-movieinfobar-col'>
+                    <FontAwesomeIcon className='fa-time' name='clock-o' size='2x'/>
+    <span className ='rmdb-movieinfobar-info'>Running time:{calcTime(props.time)}</span>
+
+                </div>
+          <div className='rmdb-movieinfobar-col'>
+              <FontAwesomeIcon className='fa-revenue' name='ticket' size ='2x'/>
+    <span className='rmdb-movieinfobar-info'>Revenue:{convertMoney(props.revenue)}</span>
+
+              </div>      
+
+            </div>
+            
         </div>
     )
 }
