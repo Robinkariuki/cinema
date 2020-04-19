66185f23bfcfbbaaf042c8696605a27aa2410e18
Robinkariuki <robinkaris7@gmail.com>
2020-04-19 12:20:24 +0300

    made changes to movie info bar

--- src/components/elements/MovieInfoBar/MovieInfoBar.js
+++ src/components/elements/MovieInfoBar/MovieInfoBar.js
@@ -8,16 +8,21 @@ const MovieInfoBar = ()=>{
     return(
         <div className='rmdb-movieinfobar'>
             <div className='rmdb-movieinfobar-content'>
-                <div className='rmdb-movieinfobar-col'>
+                <div className='rmdb-movieinfobar-content-col'>
                     <FontAwesomeIcon className='fa-time' name='clock-o' size='2x'/>
     <span className ='rmdb-movieinfobar-info'>Running time:{calcTime(props.time)}</span>
 
                 </div>
-          <div className='rmdb-movieinfobar-col'>
-              <FontAwesomeIcon className='fa-revenue' name='ticket' size ='2x'/>
-    <span className='rmdb-movieinfobar-info'>Revenue:{convertMoney(props.revenue)}</span>
+          <div className='rmdb-movieinfobar-content-col'>
+              <FontAwesomeIcon className='fa-budget' name='money' size ='2x'/>
+    <span className='rmdb-movieinfobar-info'>Budget:{convertMoney(props.budget)}</span>
 
-              </div>      
+              </div>
+              <div className ='rmdb-movieinfobar-content-col'>
+                  <FontAwesomeIcon className='fa-revenue' name='ticket' size='2x'/>
+                  <span className ='rmdb-movieinfobar-info'> Revenue: {convertMoney(props.revenue)}</span>
+                  
+                  </div>      
 
             </div>
             
