cbe632650c7ca554c6a2012f68668eff217a6d86
Robinkariuki <robinkaris7@gmail.com>
2021-03-26 14:28:36 +0300

    Updates

---
+++ precache-manifest.ae12a5716905d8219e669d0507c73f4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddbd469ef4c897beb22185dc4a714cac",
+    "url": "/cinema/index.html"
+  },
+  {
+    "revision": "511a2202b09dc6778266",
+    "url": "/cinema/static/css/main.a6055e02.chunk.css"
+  },
+  {
+    "revision": "92bfc7eaac4db2d36f15",
+    "url": "/cinema/static/js/2.2e1f2dca.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cinema/static/js/2.2e1f2dca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "511a2202b09dc6778266",
+    "url": "/cinema/static/js/main.84ab46cc.chunk.js"
+  },
+  {
+    "revision": "98c73c1b60474db2c533",
+    "url": "/cinema/static/js/runtime-main.153c47d9.js"
+  }
+]);
